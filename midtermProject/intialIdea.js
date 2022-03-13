a38e7663f6971b605a636b162403d0851ba20f6b
ayeshaahmed13 <98395837+ayeshaahmed13@users.noreply.github.com>
2022-03-14 01:32:13 +0400

    Update intialIdea.js

--- midtermProject/intialIdea.js
+++ midtermProject/intialIdea.js
@@ -1,92 +1,121 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
+// defines parameters for a three into three grid
+let board = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+];
 
-// Minesweeper
-// Video: https://youtu.be/LFU5ZlrR21E
+let players = ['X', 'O'];// allows two symbols to be input
 
-function make2DArray(cols, rows) {
-  var arr = new Array(cols);
-  for (var i = 0; i < arr.length; i++) {
-    arr[i] = new Array(rows);
-  }
-  return arr;
-}
-
-var grid;
-var cols;
-var rows;
-var w = 20;
-
-var totalBees = 30;
+// defines a condition for two players 
+let currentPlayer;
+let available = [];
 
 function setup() {
-  createCanvas(401, 401);
-  cols = floor(width / w);
-  rows = floor(height / w);
-  grid = make2DArray(cols, rows);
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j] = new Cell(i, j, w);
-    }
-  }
-
-  // Pick totalBees spots
-  var options = [];
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      options.push([i, j]);
-    }
-  }
-
-
-  for (var n = 0; n < totalBees; n++) {
-    var index = floor(random(options.length));
-    var choice = options[index];
-    var i = choice[0];
-    var j = choice[1];
-    // Deletes that spot so it's no longer an option
-    options.splice(index, 1);
-    grid[i][j].bee = true;
-  }
-
-
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j].countBees();
-    }
-  }
-
-}
-
-function gameOver() {
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j].revealed = true;
+  createCanvas(400, 400);
+  frameRate(30);
+  // parameters that define the first randomised move
+  currentPlayer = floor(random(players.length));
+  for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+      available.push([i, j]);
     }
   }
 }
 
-function mousePressed() {
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      if (grid[i][j].contains(mouseX, mouseY)) {
-        grid[i][j].reveal();
+function equals3(a, b, c) {
+  return (a == b && b == c && a != '');
+}
 
-        if (grid[i][j].bee) {
-          gameOver();
-        }
+function checkWinner() {
+  let winner = null;
 
-      }
+  // setups a  condition if three elments are same in the horizontal direction
+  for (let i = 0; i < 3; i++) {
+    if (equals3(board[i][0], board[i][1], board[i][2])) {
+      winner = board[i][0];
     }
   }
+
+  // setups a  condition if three elments are same in the vertical direction
+  for (let i = 0; i < 3; i++) {
+    if (equals3(board[0][i], board[1][i], board[2][i])) {
+      winner = board[0][i];
+    }
+  }
+
+  // setups a  condition if three elments are same in the diagonal direction
+  if (equals3(board[0][0], board[1][1], board[2][2])) {
+    winner = board[0][0];
+  }
+  if (equals3(board[2][0], board[1][1], board[0][2])) {
+    winner = board[2][0];
+  }
+
+  // if none of the above conditions are fulfilled then the game is tied
+  if (winner == null && available.length == 0) {
+    return 'tie';
+  } else {
+    return winner;
+  }
+
 }
+// defines a random move in the next turn depending on the available space
+function nextTurn() {
+  let index = floor(random(available.length));
+  let spot = available.splice(index, 1)[0];
+  let i = spot[0];
+  let j = spot[1];
+  board[i][j] = players[currentPlayer];
+  currentPlayer = (currentPlayer + 1) % players.length;
+}
+
+// function mousePressed() {
+//   nextTurn(); 
+// }
 
 function draw() {
   background(255);
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j].show();
+  let w = width / 3;
+  let h = height / 3;
+  strokeWeight(4);
+
+  line(w, 0, w, height);
+  line(w * 2, 0, w * 2, height);
+  line(0, h, width, h);
+  line(0, h * 2, width, h * 2);
+
+  for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+      let x = w * i + w / 2;
+      let y = h * j + h / 2;
+      let spot = board[i][j];
+      textSize(32);
+      if (spot == players[1]) {
+        noFill();
+        ellipse(x, y, w / 2);
+      } else if (spot == players[0]) {
+        let xr = w / 4;
+        // parameters utilised to streamline the move
+        line(x - xr, y - xr, x + xr, y + xr);
+        line(x + xr, y - xr, x - xr, y + xr);
+      }
+
     }
   }
+
+  let result = checkWinner();// condition to determine a winner
+  if (result != null) {
+    noLoop();// ends the loop if winner is declared 
+    let resultP = createP('');
+    resultP.style('font-size', '32pt');// dislays result depending on the case
+    if (result == 'tie') {
+      resultP.html("Tie!")
+    } else {
+      resultP.html(`${result} wins!`);
+    }
+  } else {
+    nextTurn();// defines condition for the next play
+  }
+
 }
